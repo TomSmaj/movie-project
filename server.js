cd3dd7805401ec72bfdc9010d83a1b17c47d6e73
Meg <m.a.gallagher09@gmail.com>
2018-11-20 18:17:17 -0600

    trying to figure out style crap

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const exphbs = require('express-handlebars');
+require("dotenv").config();
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-const db = require('./models');
+const db = require("./models");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,21 +10,21 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Routes
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 const syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
@@ -32,9 +32,9 @@ if (process.env.NODE_ENV === 'test') {
 db.sequelize.sync(syncOptions).then(() => {
   app.listen(PORT, () => {
     console.log(
-      '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
-      PORT,
+      PORT
     );
   });
 });
