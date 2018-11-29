39aa6ac9d6f79f679db5a7edf70ec28d045ebe66
TomSmaj <thomas.smajstrla@gmail.com>
2018-11-29 15:14:15 -0600

    duplicates of a username cannot be added to the DB

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -2,6 +2,7 @@ const db = require("../models");
 
 module.exports = function(app) {
   // Load example page and pass in an example by id
+  /*
   app.post("/api/user/", (req, res) => {
     db.User.create({
       username: req.body.username,
@@ -10,16 +11,33 @@ module.exports = function(app) {
       res.json(dbUser);
     });
   });
+  */
 
-  app.get("/api/user/:userinfo", (req, res) => {
+  app.post("/api/user/", (req, res) => {
     db.User.findOne({
-      where: { userId: req.params.userinfo }
-    })
-      .catch(err => {
-        res.json(err);
-      })
-      .then(dbUser => {
-        res.json(dbUser);
-      });
+      where: { userId: req.body.userId }
+    }).then(dbUser => {
+      if (dbUser) {
+        console.log("there is a user");
+      } else {
+        console.log("there is no user, thus we can create one");
+        db.User.create({
+          username: req.body.username,
+          userId: req.body.userId
+        });
+      }
+    });
   });
+
+  // app.get("/api/user/:userinfo", (req, res) => {
+  //   db.User.findOne({
+  //     where: { userId: req.params.userinfo }
+  //   })
+  //     .catch(err => {
+  //       res.json(err);
+  //     })
+  //     .then(dbUser => {
+  //       res.json(dbUser);
+  //     });
+  // });
 };
