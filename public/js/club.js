42b47298d2824c85e4ac5b89248d626f49fd0b2c
megzimo <40956115+megzimo@users.noreply.github.com>
2018-11-28 21:40:33 -0600

    post request to add movie club working

--- public/js/club.js
+++ public/js/club.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   console.log("card page ready");
 });
 
-//performs a get request that navigates to the specific club page when the club card is clicked
+// performs a get request that navigates to the specific club page when the club card is clicked
 $(".discussion").on("click", function(){
   var discussionId = $(this).attr("discussionId");
   console.log("you just clicked "+ discussionId);
@@ -12,14 +12,27 @@ $(".discussion").on("click", function(){
   });
 });
 
-// // submit post
-// $("#submit_post").on("click", function(){
-//   $.ajax({
-//     type: "POST",
-//     url: url,
-//     data: data,
-//     dataType: dataType
-//   }).then(
-//     // get req to get all discussions and reload window
-//   ).fail()
-// });
+
+// To create new club forum
+
+var new_club = $("#new_club");
+var new_descr = $("#new_club_description");
+var new_admin = $("#new_admin");
+
+$("#add_club_btn").on("click", function(e) {
+  e.preventDefault();
+  
+  var createClub = {
+    name: new_club.val().trim(),
+    description: new_descr.val().trim(),
+    // admin: new_admin
+  };
+
+  newClub(createClub);
+});
+
+function newClub(newClubName) {
+  $.post("api/clubs/", newClubName, function(res) {
+    console.log(res)
+  }).then(window.location.href = "localhost:3000" )
+}
