8738043ed207c092635e8494a2f8ed55f415d57b
Kathleen Doviken <kathleendoviken@gmail.com>
2018-11-30 21:10:03 -0600

    validation for add movie

--- public/js/omdb.js
+++ public/js/omdb.js
@@ -8,12 +8,22 @@ function displayMovieInfo() {
   const discTitle = $("#discussion-title").val();
   const start = $("#start-date").val();
   const end = $("#end-date").val();
-  const apiKey = process.env.OMDB_KEY;
-  var reqString = "https://www.omdbapi.com/?apikey=" + apiKey + "&t=" + movie;
+  var reqString = "https://www.omdbapi.com/?apikey=" + "trilogy" + "&t=" + movie;
   if (date !== "") {
     reqString += "&y=" + date;
   }
 
+  if (
+    movie === "" ||
+    discussion === "" ||
+    discTitle === "" ||
+    start === "" ||
+    end === ""
+  ) {
+    $("#modal1").modal();
+    console.log("hello");
+    $("#invalidAlert").removeClass("hide");
+  }
   console.log(reqString);
 
   $.ajax({
